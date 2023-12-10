bf1b20a34c14ade17ac53604d8010290346630e9
Tuan Nguyen <tufourn@gmail.com>
2023-12-09 23:51:57 -0500

    retrain with epoch=300

--- project/project/detect.py
+++ project/project/detect.py
@@ -65,24 +65,86 @@ class Detect(Node):
 
        
         pred = self.model(img_tensor)
-        pred = non_max_suppression(pred, conf_thres=0.25, iou_thres=0.45)
+        pred = non_max_suppression(pred, conf_thres=0.1, iou_thres=0.45)
 
         det = pred[0]
         det[:, :4] = scale_boxes(img_tensor.shape[2:], det[:, :4], img.shape).round()
 
         annotator = Annotator(img, line_width=1, example=str(self.names))
         for *xyxy, conf, cls in reversed(det):
-            label = f'{self.names[int(cls)]} {conf:.2f}'
+            label = f'{self.names[int(cls)]} {conf:.1f}'
             annotator.box_label(xyxy, label, color=colors(int(cls), True))
 
         imout = annotator.result()
         cv2.imshow('Detection', imout)
         cv2.waitKey(1)
 
+def run_on_image(weights, # .pt file
+             imgname, # input image name
+             imgsz = (256,256), # inference size (height, width)
+             device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+             ):
+
+    # Load model
+    print('Loading model')
+    device = select_device(device)
+    model = DetectMultiBackend(weights, device=device, dnn=False, data='coco128.yaml', fp16=False)
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Read image
+    print(f'Reading image {imgname}')
+    im0 = cv2.imread( imgname )
+    if im0 is None:
+        print('Warning, unable to read:', imgname)
+    img = cv2.resize(im0,imgsz)  # convert to target size
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # convert to RGB
+    img = img.transpose(2,0,1)  # Pytorch needs format: channels x height x width
+    img = np.ascontiguousarray(img)
+
+    # convert image to Pytorch tensor
+    im = torch.from_numpy(img).to(model.device)
+    im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+    im /= 255  # 0 - 255 to 0.0 - 1.0
+    im = im[None]  # Add extra dimension for convolutions
+
+    print(f'Input image shape: {im.shape}' )
+    
+    # Inference
+    print('Doing inference')
+    pred = model(im)
+
+    # NMS
+    print('Doing NMS')
+    pred = non_max_suppression(pred, conf_thres=0.25, iou_thres=0.45)
+
+    print('Processing predictions')
+    # Process predictions
+    det = pred[0]   # Single image so single prediction
+    # Rescale boxes from img_size to im0 size
+    det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+
+    print('Creating annotator')    
+    annotator = Annotator(im0, line_width=1, example=str(names))
+    for *xyxy, conf, cls in reversed(det):
+        c = int(cls)  # integer class
+        label = f'{names[c]} {conf:.2f}'
+        annotator.box_label(xyxy, label, color=colors(c, True))
+
+    print('Showing output')
+    imout = annotator.result()
+    cv2.namedWindow('Detection', cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
+    cv2.resizeWindow('Detection', imout.shape[1], imout.shape[0])
+    cv2.imshow('Detection', imout)
+    cv2.waitKey()  # Press any key when complete    
+    
+    cv2.destroyAllWindows()
+
 def main(args=None):
     rclpy.init(args=args)
-    path = str(FILE.parents[2] / 'runs' / 'train' / 'exp' / 'weights' / 'best.pt')
+    path = str(FILE.parents[2] / 'runs' / 'train' / 'exp2' / 'weights' / 'best.pt')
     node = Detect(weights=path)
+    #run_on_image(weights=path, imgname=str(FILE.parents[2] / 'test_img' / 'test_img5.jpg'))
     try:
         rclpy.spin(node)
     except (SystemExit, KeyboardInterrupt):
